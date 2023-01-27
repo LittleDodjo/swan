864a56ac72b8c635ff97553d21e12fdee127d811
Oleg <idiot5452@gmail.com>
2023-01-27 10:09:50 +0500

    main build manifest
    test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="build/assets/app.729ac221.css">
+    <title>Swan</title>
 </head>
 
-@vite("./resources/css/app.css")
-@vite("./resources/js/app.js")
-
+{{--@vite("./resources/css/app.css")--}}
+{{--@vite("./resources/js/app.js")--}}
 <body>
     <div id="app"></div>
 </body>
+
+<script src="build/assets/app.c29a4ec8.js"></script>
 </html>
